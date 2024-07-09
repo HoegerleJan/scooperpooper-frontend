579909681b82c443108ed1646989cdbfd87ca8b5
Jan Hoegerle <jan.hoegerle@gmail.com>
2024-07-09 14:23:22 +0200

    Axios stuff

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { title } from "./primitives";
 import { Button } from "@nextui-org/button";
+import axios from "axios";
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
 
 const AddButton = () => {
+  const [key, setkey] = useState(0);
+  const createEntry = () => {
+    axios
+      .post("https://localhost:7073/api/UserApi/PostUser")
+      .then(function (response) {
+        // handle success
+        setkey(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
   return (
     <>
       <div className={title({ color: "pink", size: "lg", padding: "lg" })}>
         𝓝𝓮𝓾𝓮 𝓕𝓻𝓮𝓾𝓷𝓭𝓮 𝓱𝓲𝓷𝔃𝓾𝓯ü𝓰𝓮𝓷? (づ˶•༝•˶)づ♡
       </div>
-      <Button color='secondary'>
-        <div className={title({ color: "pink", size: "sm" })}>♡ 𝓝𝓮𝓾 ♡</div>
-      </Button>
+      <Popover placement='bottom' showArrow={true} color='primary'>
+        <PopoverTrigger>
+          <Button color='secondary' onClick={createEntry}>
+            <div className={title({ color: "pink", size: "sm" })}>♡ 𝓝𝓮𝓾 ♡</div>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className='px-1 py-2'>
+            <div className='text-small font-bold'>
+              Der Editkey für die neue Seite ist:
+            </div>
+            <div className='text-tiny'>{key}</div>
+          </div>
+        </PopoverContent>
+      </Popover>
     </>
   );
 };
--- components/EntryDisplay.tsx
+++ components/EntryDisplay.tsx
@@ -1,16 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { title } from "./primitives";
+import { Input } from "@nextui-org/input";
+import axios from "axios";
+import { Button } from "@nextui-org/button";
 interface EntryDisplayProps {
   entry: Entry;
 }
 
 const EntryDisplay = ({ entry }: EntryDisplayProps) => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [editkey, setEditkey] = useState(0);
+
+  const updateCard = () => {
+    const newEntry: Entry = {
+      id: entry.id,
+      user_Id: entry.user_Id,
+      first_name: firstname,
+      last_name: lastname,
+      nickname: nickname,
+    };
+    axios
+      .put(
+        "https://localhost:7073/api/EntryApi/PutEntry?editkey=" + editkey,
+        newEntry
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
   return (
     <>
-      <div className={title({ color: "pink", size: "sm"  })}>ε(´｡•᎑•`)っ 💕</div>
-      <div className={title({ color: "pink", size: "sm", padding: "lg"})}>
-        {entry.first_name + "[" + entry.nickname + "]" + entry.last_name}
-        </div>
+      <div className={title({ color: "pink", size: "sm" })}>ε(´｡•᎑•`)っ 💕</div>
+      <div className={title({ color: "pink", size: "sm", padding: "lg" })}>
+        <div>Vorname: </div>
+        <Input
+          defaultValue={entry.first_name}
+          onChange={(e) => {
+            setFirstname(e.target.value);
+          }}
+        />
+      </div>
+      <div className={title({ color: "pink", size: "sm", padding: "lg" })}>
+        <div>Nachname: </div>
+        <Input
+          defaultValue={entry.last_name}
+          onChange={(e) => {
+            setLastname(e.target.value);
+          }}
+        />
+      </div>
+      <div className={title({ color: "pink", size: "sm", padding: "lg" })}>
+        <div>Spitzname: </div>
+        <Input
+          defaultValue={entry.nickname}
+          onChange={(e) => {
+            setNickname(e.target.value);
+          }}
+        />
+      </div>
     </>
   );
 };
